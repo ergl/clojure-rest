8f3cd63820a3190316072e145b2c07df6847ebb5
ergl <ergl@users.noreply.github.com>
2015-04-26 21:17:46 +0200

    Added comments to api tests

--- test/clojure_rest/api_test.clj
+++ test/clojure_rest/api_test.clj
@@ -4,10 +4,12 @@
             [clojure-rest.handler :refer :all]))
 
 (deftest test-api
+    ;; A GET to an available api method should return a 200 OK response
   (testing "content found"
     (let [response (app (mock/request :get "/api/events"))]
       (is (= (:status response) 200))))
-  
+
+  ;; A request to a non-existing api route should return a 404 response
   (testing "not-found asset"
     (let [response (app (mock/request :get "/api/events/00000000-0000-0000-0000-000000000000"))]
       (is (= (:status response) 404)))))
