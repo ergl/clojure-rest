b2aeca40e527cdd598e41286cade75cac68bbd13
ergl <ergl@users.noreply.github.com>
2015-04-26 22:17:25 +0200

    Modifed api tests

--- test/clojure_rest/api_test.clj
+++ test/clojure_rest/api_test.clj
@@ -7,9 +7,10 @@
     ;; A GET to an available api method should return a 200 OK response
   (testing "content found"
     (let [response (app (mock/request :get "/api/events"))]
-      (is (= (:status response) 200))))
+      (is (= (:status response) 200))
+      (is (= (get-in response [:headers "Content-Type"]) "application/json; charset=utf-8"))))
 
   ;; A request to a non-existing api route should return a 404 response
   (testing "not-found asset"
-    (let [response (app (mock/request :get "/api/events/00000000-0000-0000-0000-000000000000"))]
+    (let [response (app (mock/request :get "/api/foo"))]
       (is (= (:status response) 404)))))
