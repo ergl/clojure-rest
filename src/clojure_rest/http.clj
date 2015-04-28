bb0479372b3bd9e48fd62bd9d5f1d7194eed6373
ergl <ergl@users.noreply.github.com>
2015-04-28 15:56:45 +0200

    Added some helper http response types

--- src/clojure_rest/http.clj
+++ src/clojure_rest/http.clj
@@ -3,6 +3,15 @@
             [clojure.string :refer [upper-case]]
             [clojure.string :refer [join]]))
 
+
+;; Natural -> Response[:body nil :status Natural]
+(defn- empty-response-with-code [code]
+  (-> (response nil)
+      (status code)))
+
+;; () -> Response[:body nil :options OPTIONS]
+;; [] -> Response[:body nil :options []]
+;; [], String -> Response[:body String :options OPTIONS]
 (defn options
   ([] (options #{:options} nil))
   ([allowed] (options allowed nil))
@@ -10,10 +19,25 @@
    (-> (response body)
        (header "Allow" (join ", " (map (comp upper-case name) allowed))))))
 
+
+;; () -> Response[:body nil :options OPTIONS :status 405]
+;; [] -> Response[:body nil :options [] :status 405]
+;; [], String -> Response[:body String :options [] :status 405]
 (defn method-not-allowed [allowed]
   (-> (options allowed)
       (status 405)))
 
+
+;; () -> Response[:body nil :status 501]
 (defn not-implemented []
-  (-> (response nil)
-      (status 501)))
+	(empty-response-with-code 501))
+
+
+;; () -> Response[:body nil :status 401]
+(defn unauthorized []
+	(empty-response-with-code 401))
+
+
+;; () -> Response[:body nil :status 403]
+(defn forbidden []
+	(empty-response-with-code 403))
