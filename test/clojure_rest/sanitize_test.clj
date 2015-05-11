fc0dbe9678b4da6e390f70b5a8ec334763391002
ergl <ergl@users.noreply.github.com>
2015-05-12 01:17:52 +0200

    Moved validate tests to its own test file

--- test/clojure_rest/sanitize_test.clj
+++ test/clojure_rest/sanitize_test.clj
@@ -1,17 +1,29 @@
 (ns clojure-rest.sanitize-test
   (:require [clojure.test :refer :all]
-            [clojure-rest.sanitize :as s]
-            [clojure-rest.validate :as v]))
+            [clojure-rest.sanitize :as s]))
 
 (deftest test-sanitize
   
-  (testing "clean up a signup"
+  (testing "cleaning a valid signup map should return the same map"
     (let [user {:email "foo@foo.com" :username "username" :password "password"}]
       (is (= (s/signup-flow user) user))))
   
-  (testing "clean up a login"
+  (testing "cleaning a valid login map should return the same map"
     (let [user {:username "ayy" :password "foo"}]
       (is (= (s/login-flow user) user))))
   
-  (testing "foo"
-    (is (= (v/field-exists-in-table? "users" "username" "foo") false))))
+  (testing "extra spaces are trimmed from the map values"
+    (let [user {:email "       fo@fo.com     " :username "ayy    " :password "foo  "}]
+      (is (= ((s/signup-flow user) :email) (clojure.string/trim (user :email))))
+      (is (= ((s/login-flow user) :username) (clojure.string/trim (user :username))))
+      (is (= ((s/login-flow user) :password) (clojure.string/trim (user :password))))))
+  
+  (testing "string map is keywordized on sanitization"
+    (let [user {"email" "foo@foo.com" "username" "john" "password" "mypassword"}]
+      (is (= (s/signup-flow user) (clojure.walk/keywordize-keys user)))))
+  
+  (testing "malformed map should return a 400 Bad Request error code"
+    (let [signup-user {:email "" :username "whatever" :password "1"}
+          login-user {:username "" :password ""}]
+      (is (= (s/login-flow login-user) 400))
+      (is (= (s/signup-flow signup-user) 400)))))
---
+++ test/clojure_rest/validate_test.clj
@@ -0,0 +1,11 @@
+(ns clojure-rest.validate-test
+  (:require [clojure.test :refer :all]
+            [clojure-rest.validate :as v]))
+
+(deftest test-validate
+  
+  (testing "finding a non-existing user"
+    (is (= (v/field-exists-in-table? "users" "username" "foo") false)))
+  
+  (testing "finding a non-existing email"
+    (is (= (v/field-exists-in-table? "users" "email" "null@null.null") false))))
