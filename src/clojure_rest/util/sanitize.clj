86508cd45fbb09803a748b44377698ec4222d880
ergl <ergl@users.noreply.github.com>
2015-05-14 00:34:16 +0200

    Cleaned up sanitize, removed user logic from it.
    A lot of sanitize functions got moved either to user or error.

--- src/clojure_rest/util/sanitize.clj
+++ src/clojure_rest/util/sanitize.clj
@@ -1,6 +1,5 @@
 (ns clojure-rest.util.sanitize
-  (:require [clojure.string :refer [trim]]
-            [clojure.walk :refer [keywordize-keys]]))
+  (:require [clojure.string :refer [trim]]))
 
 ;; Error handling with a tuple [Value Error]
 ;; In our case, the value is a keywordized map coming from the client
@@ -8,37 +7,10 @@
 ;; for data inconsistencies, fill default missing values and otherwise
 ;; clean the data for model and database handling.
 
-;; (f [{}?, Error?]) -> Either<(f val)|[nil Error]>
-;; Either execute (f val) or bind the error
-;; If the error is present in the signature
-(defn bind-error [f [val err]]
-  (if (nil? err)
-    (f val)
-    [nil err]))
-
-
-;; Turns:
-;; (>>= (f val) g)
-;; Into:
-;; (->> (f val) (bind-error g))
-(defmacro >>= [val & fns]
-  (let [fns (for [f fns] `(bind-error ~f))]
-    `(->> [~val nil]
-          ~@fns)))
-
-
 ;; {} :key -> {}
 ;; Trims space on {{} :key}
 (defn- trim-in [params k]
   (assoc params k (trim (params k))))
-
-
-;; {} -> [{}?, Error?]
-;; Checks if (params :username) is non-empty
-(defn clean-username [params]
-  (if (empty? (params :username))
-    [nil 400]
-    [(trim-in params :username) nil]))
 
 
 ;; {} -> [{}?, Error?]
@@ -50,21 +22,16 @@
 
 
 ;; {} -> [{}?, Error?]
+;; Checks if (params :username) is non-empty
+(defn clean-username [params]
+  (if (empty? (params :username))
+    [nil 400]
+    [(trim-in params :username) nil]))
+
+
+;; {} -> [{}?, Error?]
 ;; Checks if (params :password) is non-empty
 (defn clean-password [params]
   (if (empty? (params :password))
     [nil 400]
     [(trim-in params :password) nil]))
-
-
-;; [{}?, Error?] -> Either<{}|Error>
-;; Extracts the value from the optional map
-;; If there is no value present, extract the error
-(defn wrap-error [[val err]]
-  (if (nil? err) val err))
-
-
-;; {} -> [{}, nil]
-;; Adaptor from map to error tuple
-(defn bind-to [params]
-  [params nil])
--- src/clojure_rest/util/validate.clj
+++ src/clojure_rest/util/validate.clj
@@ -1,6 +1,7 @@
 (ns clojure-rest.util.validate
   (:require [clojure.java.jdbc :as sql]
-            [clojure-rest.data.db :as db]))
+            [clojure-rest.data.db :as db]
+            [clojure-rest.util.error :refer [bind-to]]))
 
 ;; String String String -> Boolean
 ;; Checks if the value exists in the given row of the given table
@@ -10,3 +11,11 @@
                               (sql/with-query-results results
                                                       [(str "select 1 from " table " where " row " = ?") value]
                                                       (into {} results))))))
+
+
+;; {} key fn -> Boolean
+;; Check (fn {{} :key})
+(defn check-field [params field pred]
+  (if (pred (params field))
+    [nil 400]
+    (bind-to params)))
