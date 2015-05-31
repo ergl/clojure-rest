fc166f153bc2d834386d370f11f0fc6eb4f9655b
ergl <ergl@users.noreply.github.com>
2015-05-31 15:25:09 +0200

    Cleaned up map.js
    Added map module

--- resources/public/scripts/map.js
+++ resources/public/scripts/map.js
@@ -1,56 +1,80 @@
-// () -> ()
-function initialize() {
-  var mapOptions = {
-    center: { lat: 40.417, lng: -3.702}, // Puerta del Sol, Madrid
-    zoom: 10,
-    disableDefaultUI: true
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+var map =  (function() {
+  "use strict";
 
-  setupMarkers(map);
+  var eventList = [];
 
-  // gets user coordinates on right click
-  google.maps.event.addListener(map, 'rightclick', function(event) {
-    togglePane(PaneEnum.create);
-    var latitude = event.latLng.lat();
-    var longitude = event.latLng.lng();
-    console.log(latitude + ', ' + longitude);
-  });
-}
+  // {} -> google.maps.InfoWindow
+  // Make a personalized InfoWindow with the given configuration
+  function makeContent(config) {
+    var view = "<div class='info-window'><h2><a href='#' id='info-link' onclick='togglePane(PaneEnum.event)'>{{title}}</a></h2><p>{{attending}} user(s) are going.</p></div>";
+    var infoContent = Mustache.render(view, config);
+    return new google.maps.InfoWindow({
+      content: infoContent
+    });
+  }
 
-// google.maps.Map -> ()
-// Create the events and draw them to the given map
-function setupMarkers(mapObject) {
-  $.ajax({
-    type: "GET",
-    url: "api/events",
-    datatype: "json",
-    success: function (response) {
-      for (var i = 0; i < response.length; i++) {
-        var eventMarker = new google.maps.Marker({
-          position: new google.maps.LatLng(response[i].latitude, response[i].longitude),
-          map: mapObject
-        });
-        var config = {
-          title: response[i].title,
-          attending: response[i].attending
-        };
-        google.maps.event.addListener(eventMarker, 'click', function() {
-          makeContent(config).open(mapObject, eventMarker);
-        });
+  // google.maps.Map -> ()
+  // Create the events and draw them to the given map
+  function setupMarkers(mapObject) {
+    $.ajax({
+      type: "GET",
+      url: "api/events",
+      datatype: "json",
+      success: function (response) {
+        for (var i = 0; i < response.length; i++) {
+          var event = {
+            id: response[i].eventsid,
+            title: response[i].title,
+            attending: response[i].attending,
+            latitude: response[i].latitude,
+            longitude: response[i].longitude
+          };
+
+          var eventMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(event.latitude, event.longitude),
+            map: mapObject
+          });
+
+          var config = {
+            title: event.title,
+            attending: event.attending
+          };
+
+          eventList.push(event);
+
+          google.maps.event.addListener(eventMarker, 'click', function() {
+            makeContent(config).open(mapObject, eventMarker);
+          });
+        }
       }
-    }
-  });
-}
+    });
+  }
 
-// {} -> google.maps.InfoWindow
-// Make a personalized InfoWindow with the given configuration
-function makeContent(config) {
-  var view = "<div class='info-window'><h2><a href='#' id='info-link' onclick='togglePane(PaneEnum.event)'>{{title}}</a></h2><p>{{attending}} user(s) are going.</p></div>";
-  var infoContent = Mustache.render(view, config);
-  return new google.maps.InfoWindow({
-    content: infoContent
-  });
-}
+  // () -> ()
+  // Creates the map object and assigns the event markers
+  function initialize() {
+    var mapoptions = {
+      center: { lat: 40.417, lng: -3.702}, // Puerta del Sol, Madrid
+      zoom: 10,
+      disableDefaultUI: true
+    };
 
-google.maps.event.addDomListener(window, 'load', initialize);
+    var mapCanvas = new google.maps.Map(document.getElementById('map-canvas'), mapoptions);
+
+    setupMarkers(mapCanvas);
+
+    google.maps.event.addListener(mapCanvas, 'rightclick', function (event) {
+      togglePane(PaneEnum.create);
+      var latitude = event.latLng.lat();
+      var longitude = event.latLng.lng();
+      console.log(latitude + ', '  + longitude);
+    });
+  }
+
+  return {
+    initialize: initialize()
+  };
+
+}());
+
+google.maps.event.addDomListener(window, 'load', map.initialize);
