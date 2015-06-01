716afe2c00f6522c3b7e9033229fcc041b73f7a3
ergl <ergl@users.noreply.github.com>
2015-06-02 01:55:12 +0200

    Whitespace fix

--- resources/public/scripts/login-handler.js
+++ resources/public/scripts/login-handler.js
@@ -1,98 +1,98 @@
 var LoginHandler = (function() {
-    "use strict";
+	"use strict";
 
-    // () -> ()
-    // Gets the values from the login form.
-    var loginSubmit = function () {
-        var user = document.getElementById('login-user-text').value;
-        var pass = document.getElementById('login-pass-text').value;
-        validateLoginCredentials(user, pass);
-    };
+	// () -> ()
+	// Gets the values from the login form.
+	var loginSubmit = function () {
+		var user = document.getElementById('login-user-text').value;
+		var pass = document.getElementById('login-pass-text').value;
+		validateLoginCredentials(user, pass);
+	};
 
-    // String String -> ()
-    // Alerts the user if the user / pass combination is wrong
-    // Otherwise hides the overlay and continues to the site
-    // TODO: If user is a moderator, show the mod queue link maybe?
-    var validateLoginCredentials = function(user, pass) {
-        $.ajax({
-            type: "POST",
-            url: "api/auth",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({username: user, password: pass}),
-            datatype: "json",
-            success: function(response) {
-                localStorage.setItem('accessToken', response.token);
-                Overlays.toggleLoginOverlay();
-                toggleLoggedClass();
-            },
-            statusCode: {
-                400: function() {
-                    alert("Uh oh, something went wrong. Please try again!");
-                },
-                401: function() {
-                    alert("Bad username / password combination!");
-                }
-            }
-        })
-    };
+	// String String -> ()
+	// Alerts the user if the user / pass combination is wrong
+	// Otherwise hides the overlay and continues to the site
+	// TODO: If user is a moderator, show the mod queue link maybe?
+	var validateLoginCredentials = function(user, pass) {
+		$.ajax({
+			type: "POST",
+			url: "api/auth",
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify({username: user, password: pass}),
+			datatype: "json",
+			success: function(response) {
+				localStorage.setItem('accessToken', response.token);
+				Overlays.toggleLoginOverlay();
+				toggleLoggedClass();
+			},
+			statusCode: {
+				400: function() {
+					alert("Uh oh, something went wrong. Please try again!");
+				},
+				401: function() {
+					alert("Bad username / password combination!");
+				}
+			}
+		})
+	};
 
-    // () -> ()
-    // Gets the values from the signup form.
-    var signupSubmit = function () {
-        var email = document.getElementById('signup-email-text').value;
-        var user = document.getElementById('signup-user-text').value;
-        var pass = document.getElementById('signup-pass-text').value;
-        validateSignupCredentials(email, user,pass);
-    };
+	// () -> ()
+	// Gets the values from the signup form.
+	var signupSubmit = function () {
+		var email = document.getElementById('signup-email-text').value;
+		var user = document.getElementById('signup-user-text').value;
+		var pass = document.getElementById('signup-pass-text').value;
+		validateSignupCredentials(email, user,pass);
+	};
 
 
-    // String String String -> ()
-    // Alerts the user if the user / email / pass combination is wrong
-    // Otherwise hides the overlay and continues to the site
-    // TODO: Store the username and profile pic somewhere
-    var validateSignupCredentials = function (email, user, pass) {
-        $.ajax({
-            type: "POST",
-            url: "api/users",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({email: email, username: user, password: pass}),
-            datatyse: "json",
-            success: function(response) {
-                validateLoginCredentials(user, pass);
-            },
-            statusCode: {
-                400: function() {
-                    alert("Seems like something went wrong, please try a different email / username");
-                },
-                500: function() {
-                    alert("Uh oh. Something went really wrong!");
-                }
-            }
-        })
-    };
+	// String String String -> ()
+	// Alerts the user if the user / email / pass combination is wrong
+	// Otherwise hides the overlay and continues to the site
+	// TODO: Store the username and profile pic somewhere
+	var validateSignupCredentials = function (email, user, pass) {
+		$.ajax({
+			type: "POST",
+			url: "api/users",
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify({email: email, username: user, password: pass}),
+			datatyse: "json",
+			success: function(response) {
+				validateLoginCredentials(user, pass);
+			},
+			statusCode: {
+				400: function() {
+					alert("Seems like something went wrong, please try a different email / username");
+				},
+				500: function() {
+					alert("Uh oh. Something went really wrong!");
+				}
+			}
+		})
+	};
 
-    // () -> ()
-    // Toggles the logged-in class, meaning that we now present a logged-in interface
-    var toggleLoggedClass = function () {
-        $("#container").toggleClass('logged-in');
-    };
+	// () -> ()
+	// Toggles the logged-in class, meaning that we now present a logged-in interface
+	var toggleLoggedClass = function () {
+		$("#container").toggleClass('logged-in');
+	};
 
-    return {
-        loginSubmit: function() {
-            loginSubmit()
-        },
-        signupSubmit: function() {
-            signupSubmit()
-        }
-    }
+	return {
+		loginSubmit: function() {
+			loginSubmit()
+		},
+		signupSubmit: function() {
+			signupSubmit()
+		}
+	}
 }());
 
 $(function () {
-    $("#login-submit-button").click(function() {
-        LoginHandler.loginSubmit();
-    });
+	$("#login-submit-button").click(function() {
+		LoginHandler.loginSubmit();
+	});
 
-    $("#signup-submit-button").click(function() {
-        LoginHandler.signupSubmit();
-    });
+	$("#signup-submit-button").click(function() {
+		LoginHandler.signupSubmit();
+	});
 });
--- resources/public/scripts/main.js
+++ resources/public/scripts/main.js
@@ -1,106 +1,106 @@
 var Overlays = (function() {
-  "use strict";
+	"use strict";
 
-  var classList = document.getElementById('container').classList;
+	var classList = document.getElementById('container').classList;
 
-  var toggleUserPane = function() {
-    classList.toggle('show-user-sidebar');
-  };
+	var toggleUserPane = function() {
+		classList.toggle('show-user-sidebar');
+	};
 
-  var toggleEventPane = function () {
-    classList.toggle('show-event-sidebar');
-  };
+	var toggleEventPane = function () {
+		classList.toggle('show-event-sidebar');
+	};
 
-  var toggleCreatePane = function () {
-    classList.toggle('show-create-sidebar');
-  };
+	var toggleCreatePane = function () {
+		classList.toggle('show-create-sidebar');
+	};
 
-  var toggleMenu = function () {
-    classList.toggle('show-main-sidebar');
-  };
+	var toggleMenu = function () {
+		classList.toggle('show-main-sidebar');
+	};
 
-  var toggleLoginOverlay = function() {
-    $("#container").toggleClass('show-login-overlay');
-    $("#login-user-text").focus();
-  };
+	var toggleLoginOverlay = function() {
+		$("#container").toggleClass('show-login-overlay');
+		$("#login-user-text").focus();
+	};
 
-  // Switches between the login and the signup pane inside the overlay box
-  var toggleLoginCredentialsPane = function () {
-    $("#login-overlay").toggleClass('show-signup-pane');
-    document.getElementById(
-        (document.getElementById('login-overlay').classList.contains('show-signup-pane'))
-            ? 'signup-email-text' : 'login-user-text').focus();
-  };
+	// Switches between the login and the signup pane inside the overlay box
+	var toggleLoginCredentialsPane = function () {
+		$("#login-overlay").toggleClass('show-signup-pane');
+		document.getElementById(
+				(document.getElementById('login-overlay').classList.contains('show-signup-pane'))
+						? 'signup-email-text' : 'login-user-text').focus();
+	};
 
-  return {
-    toggleLoginOverlay: function() {toggleLoginOverlay()},
-    toggleLoginCredentialsPane: function() {toggleLoginCredentialsPane()},
-    toggleUserPane: function() {toggleUserPane()},
-    toggleEventPane: function() {toggleEventPane()},
-    toggleCreatePane: function() {toggleCreatePane()},
-    toggleMenu: function() {toggleMenu()}
-  }
+	return {
+		toggleLoginOverlay: function() {toggleLoginOverlay()},
+		toggleLoginCredentialsPane: function() {toggleLoginCredentialsPane()},
+		toggleUserPane: function() {toggleUserPane()},
+		toggleEventPane: function() {toggleEventPane()},
+		toggleCreatePane: function() {toggleCreatePane()},
+		toggleMenu: function() {toggleMenu()}
+	}
 
 }());
 
 $(function() {
 
-  var ESC_KEY_CODE = 27;
-  document.addEventListener('keydown', function(e) {
-    e = e || window.event;
-    var classList = document.getElementById('container').classList;
-    if (e.keyCode == ESC_KEY_CODE) {
-      if (classList.contains('show-login-overlay')) {
-        Overlays.toggleLoginOverlay();
-      } else if (classList.contains('show-event-sidebar')) {
-        Overlays.toggleEventPane();
-      } else if (classList.contains('show-user-sidebar')) {
-        Overlays.toggleUserPane();
-      } else if (classList.contains('show-create-sidebar')) {
-        Overlays.toggleCreatePane();
-      }
-    }
-  }, false);
+	var ESC_KEY_CODE = 27;
+	document.addEventListener('keydown', function(e) {
+		e = e || window.event;
+		var classList = document.getElementById('container').classList;
+		if (e.keyCode == ESC_KEY_CODE) {
+			if (classList.contains('show-login-overlay')) {
+				Overlays.toggleLoginOverlay();
+			} else if (classList.contains('show-event-sidebar')) {
+				Overlays.toggleEventPane();
+			} else if (classList.contains('show-user-sidebar')) {
+				Overlays.toggleUserPane();
+			} else if (classList.contains('show-create-sidebar')) {
+				Overlays.toggleCreatePane();
+			}
+		}
+	}, false);
 
-  $("#create-button").click(function () {
-    Overlays.toggleCreatePane();
-  });
+	$("#create-button").click(function () {
+		Overlays.toggleCreatePane();
+	});
 
-  $("#event-close-arrow").click(function () {
-    Overlays.toggleEventPane();
-  });
+	$("#event-close-arrow").click(function () {
+		Overlays.toggleEventPane();
+	});
 
-  $("#user-close-arrow").click(function () {
-    Overlays.toggleUserPane();
-  });
+	$("#user-close-arrow").click(function () {
+		Overlays.toggleUserPane();
+	});
 
-  $("#create-close-arrow").click(function () {
-    Overlays.toggleCreatePane();
-  });
+	$("#create-close-arrow").click(function () {
+		Overlays.toggleCreatePane();
+	});
 
-  $(".comment-user").click(function () {
-    Overlays.toggleUserPane();
-  });
+	$(".comment-user").click(function () {
+		Overlays.toggleUserPane();
+	});
 
-  $("#main-sidebar").mouseover(function () {
-    Overlays.toggleMenu();
-  }).mouseout(function () {
-    Overlays.toggleMenu();
-  });
+	$("#main-sidebar").mouseover(function () {
+		Overlays.toggleMenu();
+	}).mouseout(function () {
+		Overlays.toggleMenu();
+	});
 
-  $("#login-close-icon").click(function() {
-    Overlays.toggleLoginOverlay();
-  });
+	$("#login-close-icon").click(function() {
+		Overlays.toggleLoginOverlay();
+	});
 
-  $("#login-button-wrapper").click(function() {
-    Overlays.toggleLoginOverlay();
-  });
+	$("#login-button-wrapper").click(function() {
+		Overlays.toggleLoginOverlay();
+	});
 
-  $("#signup-link").click(function() {
-    Overlays.toggleLoginCredentialsPane();
-  });
+	$("#signup-link").click(function() {
+		Overlays.toggleLoginCredentialsPane();
+	});
 
-  $("#login-link").click(function() {
-    Overlays.toggleLoginCredentialsPane();
-  });
+	$("#login-link").click(function() {
+		Overlays.toggleLoginCredentialsPane();
+	});
 });
