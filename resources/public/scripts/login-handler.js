7c818a385e534257f95de073a390b402c3f5fd4d
ergl <ergl@users.noreply.github.com>
2015-06-01 23:58:56 +0200

    Client log in and sign up is now working
    TODO: Add validation before POSTing, store the user username and profile image, etc

--- resources/public/scripts/login-handler.js
+++ resources/public/scripts/login-handler.js
@@ -1,6 +1,30 @@
 // All login-related functions go here
 
+$(function (){
+    $("#login-close-icon").click(function() {
+        toggleLoginOverlay();
+    });
 
+    $("#login-button-wrapper").click(function() {
+        toggleLoginOverlay();
+    });
+
+    $("#signup-link").click(function() {
+      toggleCredentialsPane();
+    });
+
+    $("#login-link").click(function() {
+        toggleCredentialsPane();
+    });
+
+    $("#login-submit-button").click(function() {
+        loginSubmit();
+    });
+
+    $("#signup-submit-button").click(function() {
+        signupSubmit();
+    });
+});
 
 // () -> ()
 // Shows the login dialog, darkens the background
@@ -20,48 +44,71 @@ function toggleCredentialsPane() {
 
 // () -> ()
 // Gets the values from the login form.
-// Alerts the user if the user / pass combination is wrong, otherwise hides the overlay and continues to the site
 function loginSubmit() {
 	var user = document.getElementById('login-user-text').value;
 	var pass = document.getElementById('login-pass-text').value;
-	if (validateLoginCredentials(user, pass)) {
-		if (user === 'mod' && pass === 'mod') {
-			window.open('mod-panel.html', '_blank');
-		}
-		toggleLoginOverlay();
-		toggleLoggedClass();
-	} else {
-		alert("Bad user/pass combination!");
-	}
+    validateLoginCredentials(user, pass);
 }
 
-// String String -> Boolean
-// Returns true if the login credentials are valid
-// TODO: Actually do something
+// String String -> ()
+// Alerts the user if the user / pass combination is wrong
+// Otherwise hides the overlay and continues to the site
+// TODO: If user is a moderator, show the mod queue link maybe?
 function validateLoginCredentials(user, pass) {
-	return true;
+	$.ajax({
+        type: "POST",
+        url: "api/auth",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({username: user, password: pass}),
+        datatype: "json",
+        success: function(response) {
+            localStorage.setItem('accessToken', response.token);
+            toggleLoginOverlay();
+            toggleLoggedClass();
+        },
+        statusCode: {
+            400: function() {
+                alert("Uh oh, something went wrong. Please try again!");
+            },
+            401: function() {
+                alert("Bad username / password combination!");
+            }
+        }
+    })
 }
 
 // () -> ()
 // Gets the values from the signup form.
-// Alerts the user if the user / email / pass combination is wrong, otherwise hides the overlay and continues to the site
 function signupSubmit() {
 	var email = document.getElementById('signup-email-text').value;
 	var user = document.getElementById('signup-user-text').value;
 	var pass = document.getElementById('signup-pass-text').value;
-	if (validateSignupCredentials(email, user, pass)) {
-		toggleLoginOverlay();
-		toggleLoggedClass();
-	} else {
-		alert("Uh, oh. Something went bad!");
-	}
+    validateSignupCredentials(email, user,pass);
 }
 
-// String String String -> Boolean
-// Returns true if the signup credentials are valid
-// TODO: Actually do something
+// String String String -> ()
+// Alerts the user if the user / email / pass combination is wrong
+// Otherwise hides the overlay and continues to the site
+// TODO: Store the username and profile pic somewhere
 function validateSignupCredentials(email, user, pass) {
-	return true;
+	$.ajax({
+        type: "POST",
+        url: "api/users",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({email: email, username: user, password: pass}),
+        datatyse: "json",
+        success: function(response) {
+            validateLoginCredentials(user, pass);
+        },
+        statusCode: {
+            400: function() {
+                alert("Seems like something went wrong, please try a different email / username");
+            },
+            500: function() {
+                alert("Uh oh. Something went really wrong!");
+            }
+        }
+    })
 }
 
 // () -> ()
