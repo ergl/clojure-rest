7e63f8b1213cfd1624dc1adf2b6e186cefb9aa64
ergl <ergl@users.noreply.github.com>
2015-08-10 13:39:11 +0200

    Fix accessing bad elements of the geocode API

--- resources/public/scripts/geocode.js
+++ resources/public/scripts/geocode.js
@@ -22,8 +22,8 @@ var Utils = (function () {
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode({"address": address}, function (results, status) {
             if (status === google.maps.GeocoderStatus.OK) {
-                var latitude = results[0].geometry.location.A;
-                var longitude = results[0].geometry.location.F;
+                var latitude = results[0].geometry.location.lat();
+                var longitude = results[0].geometry.location.lng();
                 if (callback && typeof(callback) === 'function') {
                     callback(latitude, longitude);
                 } else {
